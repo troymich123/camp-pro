ddf391a5cdca3b6fe7f24c926ffca8dfcf485741
troymich123 <90191399+troymich123@users.noreply.github.com>
2022-07-05 17:29:00 +0900

    rubocop修正

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,8 +1,8 @@
 class Brand < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: 'Coleman' }, 
-    { id: 3, name: 'THE NORTH FACE' }, 
+    { id: 2, name: 'Coleman' },
+    { id: 3, name: 'THE NORTH FACE' },
     { id: 4, name: 'GREGORY' },
     { id: 5, name: 'Snow Peak' },
     { id: 6, name: 'CAPTAIN STAG' }
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,8 @@
 class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: 'テント、タープ' }, 
-    { id: 3, name: 'ライト、ランタン' }, 
+    { id: 2, name: 'テント、タープ' },
+    { id: 3, name: 'ライト、ランタン' },
     { id: 4, name: 'ストーブ、コンロ' },
     { id: 5, name: '寝袋、寝具' },
     { id: 6, name: 'その他' }
