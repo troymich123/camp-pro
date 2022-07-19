a688cc7c437229514cb575778135acdd8be52397
troymich123 <90191399+troymich123@users.noreply.github.com>
2022-07-19 21:37:16 +0900

    機能完成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,14 @@
 class LikesController < ApplicationController
   def create
+    @post = Post.find(params[:post_id])
     @post_like = Like.new(user_id: current_user.id, post_id: params[:post_id])
     @post_like.save
-    redirect_to root_path
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
     @post_like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
     @post_like.destroy
-    redirect_to root_path
   end
 
 end
