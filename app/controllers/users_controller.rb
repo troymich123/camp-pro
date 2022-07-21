ed0d81db3f3a6bb8a66d2c2c38dff1caed57516d
troymich123 <90191399+troymich123@users.noreply.github.com>
2022-07-21 22:28:05 +0900

    リンク修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,13 +21,19 @@ class UsersController < ApplicationController
   end
 
   def follows
-    user = User.find(params[:id])
-    @users = user.following_user.page(params[:page]).per(3).reverse_order
+    @user = User.find(params[:id])
+    @users = @user.following_user.reverse_order
+    @following_users = @user.following_user
+    @follower_users = @user.follower_user
+    @posts = @user.posts.page(params[:page]).reverse_order
   end
   
   def followers
-    user = User.find(params[:id])
-    @users = user.follower_user.page(params[:page]).per(3).reverse_order
+    @user = User.find(params[:id])
+    @users = @user.follower_user.reverse_order
+    @following_users = @user.following_user
+    @follower_users = @user.follower_user
+    @posts = @user.posts.page(params[:page]).reverse_order
   end
   
 end
