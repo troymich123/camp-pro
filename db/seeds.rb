0ced5e632b09d915aa9dd9b69c3330222a600f1a
troymich123 <90191399+troymich123@users.noreply.github.com>
2022-07-22 15:17:15 +0900

    seed記入、他微修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  nickname: "テストキャンパー",
+  email: "test@test",
+  password: "a000000",
+  password_confirmation: "a000000"
+)
+
+10.times do |n|
+  User.create!(
+    nickname: Faker::JapaneseMedia::DragonBall.character,
+    email: Faker::Internet.unique.email,
+    password: "a000000",
+    password_confirmation: "a000000"
+  )
+end
+
+10.times do |n|
+  User.first(2).each do |user|
+    user.posts.create!(
+      user_id: user.id,
+      category_id: Faker::Number.within(range: 2..65),
+      brand_id: Faker::Number.within(range: 2..32),
+      gearmodel: Faker::JapaneseMedia::Doraemon.gadget,
+      text: Faker::Lorem.paragraph
+    )
+  end
+end
+
+6.times do |n|
+  User.find(1) do |user|
+    user.posts.create!(
+      user_id: user.id,
+      category_id: Faker::Number.within(range: 2..65),
+      brand_id: Faker::Number.within(range: 2..32),
+      gearmodel: Faker::JapaneseMedia::Doraemon.gadget,
+      text: Faker::Lorem.paragraph,
+      image: ActiveStorage::Blob.create_and_upload!(io: File.open("app/assets/images/test.jpg"), filename:"test.jpg")
+    )
+  end
+end
