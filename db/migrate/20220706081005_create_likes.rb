cffcd72ba89db1e105795548e05218e77010875a
troymich123 <90191399+troymich123@users.noreply.github.com>
2022-07-22 20:43:59 +0900

    likesテーブル修正

--- db/migrate/20220706081005_create_likes.rb
+++ db/migrate/20220706081005_create_likes.rb
@@ -4,7 +4,7 @@ class CreateLikes < ActiveRecord::Migration[6.0]
       t.references    :user,    null: false,    foreign_key: true
       t.references    :post,    null: false,    foreign_key: true
       t.timestamps
-      t.index  [:user_id, :item_id],   unique: true
+      t.index  [:user_id, :post_id],   unique: true
     end
   end
 end
