c7e9b4ed4fd499d7a84b6d84d3f131fe9f4c7311
troymich123 <90191399+troymich123@users.noreply.github.com>
2022-07-25 22:59:34 +0900

    余分なルーティング削除

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,10 @@ module Campfuel
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
 
+    initializer(:remove_action_mailbox_and_activestorage_routes, after: :add_routing_paths) { |app|
+    app.routes_reloader.paths.delete_if {|path| path =~ /activestorage/}
+    app.routes_reloader.paths.delete_if {|path| path =~ /actionmailbox/}
+    }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
