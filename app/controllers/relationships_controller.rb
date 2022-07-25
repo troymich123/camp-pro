06305840f3ecfba213bb9bcd45304e7e16917bda
troymich123 <90191399+troymich123@users.noreply.github.com>
2022-07-25 14:47:36 +0900

    フォローによる経験値機能

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,11 +1,33 @@
 class RelationshipsController < ApplicationController
   def create
     current_user.follow(params[:user_id])
+    #経験値アップ・レベルアップ機能
+    followeduser = User.find(params[:user_id])
+    followeduser.exp_point = followeduser.exp_point + 1
+    followeduser.update(exp_point: followeduser.exp_point)
+    ranksetting = RankSetting.find_by(rank: followeduser.rank + 1)
+    if ranksetting.threshold <= followeduser.exp_point
+      followeduser.rank = followeduser.rank + 1
+      followeduser.rank_name = ranksetting.rank_name
+      followeduser.update(rank: followeduser.rank, rank_name: followeduser.rank_name)
+    end
+    #/経験値アップ・レベルアップ機能
     redirect_to request.referer
   end
 
   def destroy
     current_user.unfollow(params[:user_id])
+    #経験値ダウン・レベルダウン機能
+    followeduser = User.find(params[:user_id])
+    followeduser.exp_point = followeduser.exp_point - 1
+    followeduser.update(exp_point: followeduser.exp_point)
+    ranksetting = RankSetting.find_by(rank: followeduser.rank - 1)
+    if ranksetting.threshold <= followeduser.exp_point
+      followeduser.rank = followeduser.rank - 1
+      followeduser.rank_name = ranksetting.rank_name
+      followeduser.update(rank: followeduser.rank, rank_name: followeduser.rank_name)
+    end
+    #/経験値ダウン・レベルダウン機能
     redirect_to request.referer
   end
 end
